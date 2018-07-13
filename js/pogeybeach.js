4e3c8dfa21b2faec59e1e8890d847066b599b52f
Quoticus <38863921+Quoticus@users.noreply.github.com>
2018-07-13 13:41:04 -0300

    Adding Comments
    Also removing old commented out code.
    
    After this, it'll make sense to start building a more object oriented approach to all the functions.

--- js/pogeybeach.js
+++ js/pogeybeach.js
@@ -2,9 +2,7 @@ window.onload = function(){
 	var workBonus = 7;
 	var maxWeeks = 45;
 	let questions = $("#questions");
-	//var fillMultiplier = 0;
-	//var fillTimer = 5000;
-	//var fillEvent;
+	//Creating a data object for ease of being passed through functions.
 	let passable = {
 		work:0,
 		workBonus:workBonus,
@@ -15,6 +13,9 @@ window.onload = function(){
 		fillMultiplier:0
 	};
 
+	/**
+		Hides/Shows the question field for the nnnYn questions based on if there's "Work"
+	*/
 	$(document).click(function(){
 		if(passable.work <= 0){
 			questions.hide();
@@ -22,46 +23,48 @@ window.onload = function(){
 			questions.show();
 		}
 	});
+
+	/**
+		Checks the questions radios when they're clicked.
+	*/
 	questions.children(":radio").click(function(){
 		let radios = questions.children(":radio");
 		if(checkEntries(radios)){
-			let currentMon = parseInt($("#myProgress").text());
-			currentMon += 400 + (passable.work * workBonus);
-			$("#myProgress").text(currentMon);
-			questions[0].reset();
-			passable.work--;
-			$("#multiplier").text(passable.work);
-			updateBar(passable.work, maxWeeks);
+			calculateEarnings(passable);
 		}
 	});
+
+	/**
+		Executes when the "Work" button is activated
+	*/
 	$("#doWork").click(function(){
 		if(passable.work < maxWeeks){
 			passable.work++;
-			//console.log(passable.fillEvent);
-			//console.log(passable.fillMultiplier);
-
 			if(passable.fillEvent == 0 && passable.fillMultiplier > 0){
-				//console.log(passable.fillEvent);
-				//console.log(passable.fillMultiplier);
 				setIntervalEvent(passable);
 			}
 			$("#multiplier").text(passable.work);
 			updateBar(passable.work, maxWeeks);
 		}
 	});
+
+	/**
+		Executes when the "Auto-Work" button is activated
+	*/
 	$("#autoWork").click(function(){
 		passable.fillMultiplier++;
 		if(passable.fillEvent){
-			//clearInterval(passable.fillEvent);
 			clearPassableInterval(passable);
 		}
 		setIntervalEvent(passable);
-		console.log((passable.fillTimer/passable.fillMultiplier));
 		$("#autoFill").text(passable.fillMultiplier);
 		updateBar(passable.work, maxWeeks);
 	});
 }
 
+/**
+	Updates the Progress Bar based on currently available values.
+*/
 function updateBar(work, maxWeeks){
 	let bar = $("#bar");
 	let progressBar = $("#progressBar");
@@ -69,6 +72,9 @@ function updateBar(work, maxWeeks){
 	bar.css("width", function(){return ((progressBar.width())/maxWeeks)*work;});
 }
 
+/**
+	Iterates through the input array of radios. Only returns true if all are "Checked".
+*/
 function checkEntries(radios){
 	for(var i = 0; i < radios.length; i++){
 		if(!radios[i].checked){
@@ -78,6 +84,12 @@ function checkEntries(radios){
 	return true;
 }
 
+/**
+	Creates the interval timer.
+
+	It may be worth changing this to a setTimeout instead, and building in recursion
+	in order to make sure the process completes even if the button is used multiple times.
+*/
 function setIntervalEvent(passable){
 	let radios = passable.questions.children(":radio");
 	passable.fillEvent = setInterval(function(){
@@ -85,12 +97,14 @@ function setIntervalEvent(passable){
 	}, (passable.fillTimer/passable.fillMultiplier));
 }
 
+/**
+	Timer activated function, marks the next "false" set Radio to "true"
+	If all are checked, calls the function to reset the questions.
+*/
 function markNextRadio(radios, passable){
 	for(var i = 0; i < radios.length; i++){
 		if(!radios[i].checked){
-			console.log("Marking Next Radio");
 			radios[i].checked = true;
-
 			//this'll have to be changed to an else of the parent when a CONFIRM button is added
 			if(checkEntries(radios)){
 				calculateEarnings(passable);
@@ -100,11 +114,17 @@ function markNextRadio(radios, passable){
 	}
 }
 
+/**
+	Clears the currently set Interval, so that a new one can be set.
+*/
 function clearPassableInterval(passable){
 	clearInterval(passable.fillEvent);
 	passable.fillEvent = 0;
 }
 
+/**
+	Resets the questions, and calculates the new earnings values
+*/
 function calculateEarnings(passable){
 	let currentMon = parseInt($("#myProgress").text());
 	currentMon += 400 + (passable.work * passable.workBonus);
@@ -115,7 +135,6 @@ function calculateEarnings(passable){
 	updateBar(passable.work, passable.maxWeeks);
 	if(passable.work <= 0){
 		passable.questions.hide();
-		//clearInterval(passable.fillEvent);
 		clearPassableInterval(passable);
 	}
 }
